6b0957b513a31a977db832d80a5003107cba1964
bombasticRY <504073254@qq.com>
2019-06-17 15:13:20 +0800

    chapter2 last partition

---
+++ Chapter2/06num.py
@@ -0,0 +1,25 @@
+# 加减乘除
+print(3+2)
+print(3-2)
+print(3*2)
+print(3/2)
+
+# 指数运算
+print(3**2)
+print(10**3)
+
+# 优先级
+print(3 + 2 * 4)
+print((3 + 2) * 4)
+
+# 浮点数
+print(0.1 + 0.1)
+print(0.2 * 4)
+# 包含小数位数不确定情况
+print(0.2+0.1)
+print(3 * 0.2)
+
+# 下面包含错误信息，仔细观察，并注意解决办法
+age = 23
+# print("Happy " + age + "rd Birthday!")
+print("Happy " + str(age) + "rd Birthday!")
---
+++ Chapter2/07HMnum_practice.py
@@ -0,0 +1,12 @@
+# 2-8 Practice
+print(3 + 5)
+print(10 - 2)
+print(2 * 4)
+print(round(16 / 2))
+# print(16 // 2)
+
+# 2-9 Practice
+favorite_num = 7
+print("My Favourite Num is " + str(favorite_num) + " !")
+
+# 2-10 添加注释
